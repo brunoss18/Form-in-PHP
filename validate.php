e6b9097472aac4a2fbcaba0d34d0088863595aaa
Bruno Soares <66048360+brunoss18@users.noreply.github.com>
2022-10-12 03:17:09 -0300

    Formulário validado PHP

---
+++ validate.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+
+
+
+  $nome = addslashes($_POST['name']);
+  $email = addslashes($_POST['email']);
+  $mensagem = addslashes($_POST['message']);
+
+
+  $to = "contato@besirius.com.br";
+  $subject = "Contato - Sirius Educação";
+  $body = "Nome: " . $nome . "\r\n" .
+    "Email: " . $email . "\r\n" .
+    "Mensagem: " . $mensagem;
+  $header = "From: bruno@besirius.com.br" . "\r\n" .
+    "Reply-To: " . $email . "\e\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+  if (mail($to, $subject, $body, $header)) {
+
+    echo ("Email enviado com sucesso!");
+  } else {
+
+    echo ("O Email não pode ser enviado");
+  }
+}
+
+
+?>
+
+<!-- Projeto para praticar o uso do PHP -->
